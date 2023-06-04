8e857d4852e99cacafdd09f23e69d58347e6feb3
HAMA Barhamou <hamabarhamou@gmail.com>
2023-06-04 20:13:18 +0100

    affiche du  menue en fonction des roles de user

--- user_management/models.py
+++ user_management/models.py
@@ -18,6 +18,7 @@ class User(AbstractUser):
         ('admin', 'Administrateur'),
         ('manager', 'Gestionnaire'),
         ('employee', 'Employé'),
+        ('driver', 'driver'),
     )
 
     role = models.CharField(max_length=20, choices=ROLES)
